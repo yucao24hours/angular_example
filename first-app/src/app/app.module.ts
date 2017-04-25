8985e776d441d08f7b5b243c57793d9f1d03b0fb
Yuka Kato <satoimo.to.imo@gmail.com>
2017-04-25 09:54:24 +0900

    Add comments

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -3,18 +3,23 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// AppComponent クラスを、src/app/app.component.ts からインポート
 import { AppComponent } from './app.component';
 
 @NgModule({
+  // このアプリで利用するクラス
   declarations: [
     AppComponent
   ],
+  // このアプリで利用するモジュール
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
+  // DI 元となるクラス
   providers: [],
+  // 最初に起動するコンポーネント
   bootstrap: [AppComponent]
 })
 export class AppModule { }
