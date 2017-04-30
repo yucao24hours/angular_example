11320aa502558dc5332b3487864355b191579eda
Yuka Kato <satoimo.to.imo@gmail.com>
2017-04-30 21:44:55 +0900

    Add comments

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // HTML の出力先として、app-root を指定
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
--- first-app/src/main.ts
+++ first-app/src/main.ts
@@ -1,9 +1,11 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+// AppModule は、src/app/app.module を参照する
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// 実行モードが開発モードか運用モードか判定
 if (environment.production) {
   enableProdMode();
 }
