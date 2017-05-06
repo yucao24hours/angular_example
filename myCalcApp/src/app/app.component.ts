4b00cfda63ec365b48e3e5d8206c92887d20f075
Yuka Kato <satoimo.to.imo@gmail.com>
2017-05-07 02:09:45 +0900

    BMI 計算部分は DI するようにした

--- myCalcApp/src/app/app.component.ts
+++ myCalcApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BmiService } from './bmi.service';
 
 @Component({
   selector: 'app-root',
@@ -9,16 +10,7 @@ export class AppComponent {
   height: number; // 双方向データバインドする変数（身長）
   weight: number; // 双方向データバインドする変数（体重）
 
-  // 本来だったらこういうのは Service class にするほうがいいんだろう。
-  calcBmi(): number {
-    if(isNaN(this.height) || isNaN(this.weight)) {
-      return null;
-    }
-
-    let height_meter = this.height / 100;
-    let bmi: number = this.weight / (height_meter ** 2);
-    return bmi;
-  }
+  constructor(private bmiService: BmiService){}
 
   save(): void {
     localStorage.setItem(
--- myCalcApp/src/app/app.module.ts
+++ myCalcApp/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BmiService } from './bmi.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    BmiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
