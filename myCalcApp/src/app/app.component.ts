03b2505aad63371a4a468ea3698765f7337ea55e
Yuka Kato <satoimo.to.imo@gmail.com>
2017-05-06 19:53:16 +0900

    ビジネスロジックを実装する

--- myCalcApp/src/app/app.component.ts
+++ myCalcApp/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  initValue: number; // 元本入力欄と双方向データバインドする変数
+  rate: number; // 金利入力欄と双方向データバインドする変数
+
+  calc(): number {
+    // NaN（非数）かどうかを判定し、
+    if(isNaN(this.initValue) || isNaN(this.rate)) {
+      // 元本または利率が数字でない場合は null を返す
+      return null;
+    }
+
+    let answer: number = this.initValue;
+    for (let i = 0; i < 10; i++) {
+      // 金利計算を10回繰り返して複利計算をする
+      answer = answer * (1 + this.rate / 100);
+    }
+    return Math.floor(answer); // 計算結果を整数に変換して返す
+  }
 }
