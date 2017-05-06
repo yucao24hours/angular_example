c1b422615a0abacac8198985d627e600e63610f7
Yuka Kato <satoimo.to.imo@gmail.com>
2017-05-07 00:40:51 +0900

    BMI 計算アプリを作った

--- myCalcApp/src/app/app.component.ts
+++ myCalcApp/src/app/app.component.ts
@@ -6,35 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  initValue: number; // 元本入力欄と双方向データバインドする変数
-  rate: number; // 金利入力欄と双方向データバインドする変数
+  height: number; // 双方向データバインドする変数（身長）
+  weight: number; // 双方向データバインドする変数（体重）
 
-  calc(): number {
-    // NaN（非数）かどうかを判定し、
-    if(isNaN(this.initValue) || isNaN(this.rate)) {
-      // 元本または利率が数字でない場合は null を返す
+  // 本来だったらこういうのは Service class にするほうがいいんだろう。
+  calcBmi(): number {
+    if(isNaN(this.height) || isNaN(this.weight)) {
       return null;
     }
 
-    let answer: number = this.initValue;
-    for (let i = 0; i < 10; i++) {
-      // 金利計算を10回繰り返して複利計算をする
-      answer = answer * (1 + this.rate / 100);
-    }
-    return Math.floor(answer); // 計算結果を整数に変換して返す
-  }
-
-  // 年ごとの金額明細
-  calcArray(): number[] {
-    if (isNaN(this.initValue) || isNaN(this.rate)) {
-      return null;
-    }
-    let answer: number = this.initValue;
-    let ret: number[] = [answer];
-    for (let i = 0; i < 10; i++) {
-      answer = answer * (1 + this.rate / 100);
-      ret.push(Math.floor(answer));
-    }
-    return ret;
+    let height_meter = this.height / 100;
+    let bmi: number = this.weight / (height_meter ** 2);
+    return bmi;
   }
 }
