ed6f2be9a1f599e2f0219e914a86315d0f5da54d
Yuka Kato <satoimo.to.imo@gmail.com>
2017-05-07 09:19:27 +0900

    Get Angular to talk to Rails

--- home_library_root/home-library/src/app/app.component.ts
+++ home_library_root/home-library/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  books;
+
+  constructor(private http: Http) {
+    http.get('http://localhost:3000/books.json')
+      .subscribe(res => this.books = res.json());
+  }
 }
